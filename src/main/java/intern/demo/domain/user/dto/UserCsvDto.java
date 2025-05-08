8b8a59dba3312515b60a1b993faf89d0eac87fe8
jiyeon0926 <yeoni9094@naver.com>
2025-05-08 12:20:17 +0900

    feat: csv 파일 업로드 및 컬럼 추가
    - id 컬럼 추가 및 UUID 값 넣어줌

---
+++ src/main/java/intern/demo/domain/user/dto/UserCsvDto.java
@@ -0,0 +1,131 @@
+package intern.demo.domain.user.dto;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+public class UserCsvDto {
+
+    @Setter
+    private String id;
+
+    @CsvBindByName(column = "[shorts] agreeLicensedVideo")
+    private String shortsAgreeLicensedVideo;
+
+    @CsvBindByName(column = "[shorts] certified")
+    private String shortsCertified;
+
+    @CsvBindByName(column = "[Shorts] Commission")
+    private Integer shortsCommission;
+
+    @CsvBindByName(column = "access_token")
+    private String accessToken;
+
+    private String admin;
+
+    private String agreementPrivacy;
+
+    private String agreementTemrs;
+
+    private String alarmYn;
+
+    private String bankAccountHolder;
+
+    private String bankAccountNumber;
+
+    private String bankName;
+
+    private String bio;
+
+    private String blocked;
+
+    private String businessAccount;
+
+    private String businessBankAccountFile;
+
+    @CsvBindByName(column = "businessReg-Docu")
+    private String businessRegDocu;
+
+    private String dateOfBirth;
+
+    private String deviceOS;
+
+    private String firstSubmittion;
+
+    private List followers;
+
+    private List following;
+
+    private String gcmKey;
+
+    private String identificationNumber;
+
+    private String imgProfile;
+
+    private String joinedAt;
+
+    private String labelName;
+
+    private String laeeblyEmail;
+
+    private String laeeblyId;
+
+    private String laeeblyMemo;
+
+    private Integer laeeblyPurchasedAmount;
+
+    private Integer laeeblyPurchasedCount;
+
+    private String language;
+
+    private String leftAt;
+
+    private String leftReason;
+
+    private Integer memberIdx;
+
+    private String membership;
+
+    @CsvBindByName(column = "membershipPaid (deleted)")
+    private List membershipPaid;
+
+    private String membershipStatus;
+
+    private String name;
+
+    @CsvBindByName(column = "NFTwallet")
+    private String nftWallet;
+
+    private String nickName;
+
+    @CsvBindByName(column = "Partner")
+    private String partner;
+
+    private String payerID;
+
+    private String phoneNumber;
+
+    private List referrer;
+
+    @CsvBindByName(column = "refresh_token")
+    private String refreshToken;
+
+    private List role;
+
+    @CsvBindByName(column = "Creation Date")
+    private String creationDate;
+
+    @CsvBindByName(column = "Modified Date")
+    private String ModifiedDate;
+
+    private String email;
+
+    @CsvBindByName(column = "null")
+    private String nullColumn;
+
+    @CsvBindByName(column = "unique id")
+    private String uniqueID;
+}
