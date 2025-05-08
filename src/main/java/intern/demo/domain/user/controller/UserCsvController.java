b93968cb25caf535b7e4578e29135c6f4c805bc6
jiyeon0926 <yeoni9094@naver.com>
2025-05-08 14:51:37 +0900

    fix: 다운로드 로직 수정

--- src/main/java/intern/demo/domain/user/controller/UserCsvController.java
+++ src/main/java/intern/demo/domain/user/controller/UserCsvController.java
@@ -1,15 +1,14 @@
 package intern.demo.domain.user.controller;
 
-import intern.demo.domain.user.dto.UserCsvDto;
 import intern.demo.domain.user.service.UserCsvService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,9 +19,32 @@ public class UserCsvController {
 
     // 파일 업로드
     @PostMapping("/upload/csv")
-    public ResponseEntity<List<UserCsvDto>> uploadUserCsv(@RequestParam("file") MultipartFile file) throws IOException {
-        List<UserCsvDto> userCsvDtoList = userCsvService.processUserCsv(file);
+    public ResponseEntity<String> uploadUserCsv(@RequestParam("file") MultipartFile file) throws IOException {
+        String filePath = userCsvService.processUserCsv(file);
 
-        return new ResponseEntity<>(userCsvDtoList, HttpStatus.OK);
+        return new ResponseEntity<>(filePath, HttpStatus.OK);
+    }
+
+    // 다운로드
+    @GetMapping("/download/csv")
+    public void downloadUserCsv(@RequestParam("file") String filename, HttpServletResponse response) {
+        File file = userCsvService.getFileToDownload(filename);
+
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+
+        try (InputStream in = new FileInputStream(file);
+             OutputStream out = response.getOutputStream()) {
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("파일 다운로드 실패", e);
+        }
     }
 }
--- src/main/java/intern/demo/domain/user/service/UserCsvService.java
+++ src/main/java/intern/demo/domain/user/service/UserCsvService.java
@@ -1,42 +1,39 @@
 package intern.demo.domain.user.service;
 
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import intern.demo.domain.user.dto.UserCsvDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
 public class UserCsvService {
 
     // 업로드된 파일의 데이터 가공
-    public List<UserCsvDto> processUserCsv(MultipartFile file) {
+    public String processUserCsv(MultipartFile file) {
         try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
             CsvToBean<UserCsvDto> csvToBean = new CsvToBeanBuilder<UserCsvDto>(reader)
                     .withType(UserCsvDto.class)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
 
-            List<UserCsvDto> userCsvDtoList = csvToBean.parse();
-
-            return userCsvDtoList.stream()
+            List<UserCsvDto> userCsvDtoList = csvToBean.parse().stream()
                     .map(userCsvDto -> {
                         userCsvDto.setId(UUID.randomUUID().toString());
 
                         String formattedCreatedDate = formatDate(userCsvDto.getCreated_date());
                         userCsvDto.setCreated_date(formattedCreatedDate);
+
                         String formattedModifiedDate = formatDate(userCsvDto.getModified_date());
                         userCsvDto.setModified_date(formattedModifiedDate);
 
@@ -44,9 +41,42 @@ public class UserCsvService {
                     })
                     .collect(Collectors.toList());
 
+            // 파일로 저장
+            String filePath = saveCsvToFile(userCsvDtoList);
+
+            return filePath;
+
         } catch (IOException e) {
             throw new RuntimeException("CSV 파싱 실패", e);
         }
+    }
+
+    public File getFileToDownload(String filename) {
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            throw new RuntimeException("파일이 존재하지 않습니다.");
+        }
+
+        return file;
+    }
+
+    private String saveCsvToFile(List<UserCsvDto> userCsvDtoList) {
+        String fileName = "user_formatted.csv";
+        File csvOutputFile = new File(fileName);
+
+        try (Writer writer = new FileWriter(csvOutputFile)) {
+            StatefulBeanToCsv<UserCsvDto> beanToCsv = new StatefulBeanToCsvBuilder<UserCsvDto>(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .withSeparator(',')
+                    .withOrderedResults(true)
+                    .build();
+            beanToCsv.write(userCsvDtoList);
+        } catch (Exception e) {
+            throw new RuntimeException("CSV 파일 저장 실패", e);
+        }
+
+        return csvOutputFile.getAbsolutePath();
     }
 
     private String formatDate(String inputDate) {
