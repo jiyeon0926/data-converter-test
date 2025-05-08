bdf478afb34be5775a673e18c51c171819dbfe94
jiyeon0926 <yeoni9094@naver.com>
2025-05-08 16:04:50 +0900

    fix: 필드마다 CsvBindByName 붙임

--- src/main/java/intern/demo/domain/user/dto/UserCsvDto.java
+++ src/main/java/intern/demo/domain/user/dto/UserCsvDto.java
@@ -4,13 +4,8 @@ import com.opencsv.bean.CsvBindByName;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 public class UserCsvDto {
-
-    @Setter
-    private String id;
 
     @CsvBindByName(column = "[shorts] agreeLicensedVideo")
     private String shortsAgreeLicensedVideo;
@@ -24,96 +19,134 @@ public class UserCsvDto {
     @CsvBindByName(column = "access_token")
     private String accessToken;
 
+    @CsvBindByName(column = "admin")
     private String admin;
 
+    @CsvBindByName(column = "agreementPrivacy")
     private String agreementPrivacy;
 
+    @CsvBindByName(column = "agreementTemrs")
     private String agreementTemrs;
 
+    @CsvBindByName(column = "alarmYn")
     private String alarmYn;
 
+    @CsvBindByName(column = "bankAccountHolder")
     private String bankAccountHolder;
 
+    @CsvBindByName(column = "bankAccountNumber")
     private String bankAccountNumber;
 
+    @CsvBindByName(column = "bankName")
     private String bankName;
 
+    @CsvBindByName(column = "bio")
     private String bio;
 
+    @CsvBindByName(column = "blocked")
     private String blocked;
 
+    @CsvBindByName(column = "businessAccount")
     private String businessAccount;
 
+    @CsvBindByName(column = "businessBankAccountFile")
     private String businessBankAccountFile;
 
     @CsvBindByName(column = "businessReg-Docu")
     private String businessRegDocu;
 
+    @CsvBindByName(column = "dateOfBirth")
     private String dateOfBirth;
 
+    @CsvBindByName(column = "deviceOS")
     private String deviceOS;
 
+    @CsvBindByName(column = "firstSubmittion")
     private String firstSubmittion;
 
-    private List followers;
+    @CsvBindByName(column = "followers")
+    private String followers;
 
-    private List following;
+    @CsvBindByName(column = "following")
+    private String following;
 
+    @CsvBindByName(column = "gcmKey")
     private String gcmKey;
 
+    @CsvBindByName(column = "identificationNumber")
     private String identificationNumber;
 
+    @CsvBindByName(column = "imgProfile")
     private String imgProfile;
 
+    @CsvBindByName(column = "joinedAt")
     private String joinedAt;
 
+    @CsvBindByName(column = "labelName")
     private String labelName;
 
+    @CsvBindByName(column = "laeeblyEmail")
     private String laeeblyEmail;
 
+    @CsvBindByName(column = "laeeblyId")
     private String laeeblyId;
 
+    @CsvBindByName(column = "laeeblyMemo")
     private String laeeblyMemo;
 
+    @CsvBindByName(column = "laeeblyPurchasedAmount")
     private Integer laeeblyPurchasedAmount;
 
+    @CsvBindByName(column = "laeeblyPurchasedCount")
     private Integer laeeblyPurchasedCount;
 
+    @CsvBindByName(column = "language")
     private String language;
 
+    @CsvBindByName(column = "leftAt")
     private String leftAt;
 
+    @CsvBindByName(column = "leftReason")
     private String leftReason;
 
+    @CsvBindByName(column = "memberIdx")
     private Integer memberIdx;
 
+    @CsvBindByName(column = "membership")
     private String membership;
 
     @CsvBindByName(column = "membershipPaid (deleted)")
-    private List membershipPaid;
+    private String membershipPaid;
 
+    @CsvBindByName(column = "membershipStatus")
     private String membershipStatus;
 
+    @CsvBindByName(column = "name")
     private String name;
 
     @CsvBindByName(column = "NFTwallet")
     private String nftWallet;
 
+    @CsvBindByName(column = "nickName")
     private String nickName;
 
     @CsvBindByName(column = "Partner")
     private String partner;
 
+    @CsvBindByName(column = "payerID")
     private String payerID;
 
+    @CsvBindByName(column = "phoneNumber")
     private String phoneNumber;
 
-    private List referrer;
+    @CsvBindByName(column = "referrer")
+    private String referrer;
 
     @CsvBindByName(column = "refresh_token")
     private String refreshToken;
 
-    private List role;
+    @CsvBindByName(column = "role")
+    private String role;
 
     @Setter
     @CsvBindByName(column = "Creation Date")
@@ -123,6 +156,7 @@ public class UserCsvDto {
     @CsvBindByName(column = "Modified Date")
     private String modified_date;
 
+    @CsvBindByName(column = "email")
     private String email;
 
     @CsvBindByName(column = "null")
@@ -130,4 +164,7 @@ public class UserCsvDto {
 
     @CsvBindByName(column = "unique id")
     private String uniqueID;
+
+    @Setter
+    private String id;
 }
